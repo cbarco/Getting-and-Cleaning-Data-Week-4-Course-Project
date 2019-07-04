addedf7818394a949828e7e725eb517279ab268e
cbarco <47506234+cbarco@users.noreply.github.com>
2019-07-04 18:35:08 -0500

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,61 @@
+#run_analysis.R
+#Catalina Barco Castillo
+#Getting and Cleaning Data
+# July 2019
+
+library(data.table)
+library(dplyr)
+
+setwd("~/Desktop/JHU/Getting and cleaning data")
+
+URL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+destFile <- "CourseDataset.zip"
+if (!file.exists(destFile)){
+  download.file(URL, destfile = destFile, mode='wb')
+}
+if (!file.exists("./UCI_HAR_Dataset")){
+  unzip(destFile)
+}
+dateDownloaded <- date()
+
+setwd("~/Desktop/JHU/Getting and cleaning data/UCI HAR Dataset")
+
+ActivityTest <- read.table("./test/y_test.txt", header = F)
+ActivityTrain <- read.table("./train/y_train.txt", header = F)
+FeaturesTest <- read.table("./test/X_test.txt", header = F)
+FeaturesTrain <- read.table("./train/X_train.txt", header = F)
+SubjectTest <- read.table("./test/subject_test.txt", header = F)
+SubjectTrain <- read.table("./train/subject_train.txt", header = F)
+ActivityLabels <- read.table("./activity_labels.txt", header = F)
+FeaturesNames <- read.table("./features.txt", header = F)
+
+FeaturesData <- rbind(FeaturesTest, FeaturesTrain)
+SubjectData <- rbind(SubjectTest, SubjectTrain)
+ActivityData <- rbind(ActivityTest, ActivityTrain)
+
+names(ActivityData) <- "ActivityN"
+names(ActivityLabels) <- c("ActivityN", "Activity")
+
+Activity <- left_join(ActivityData, ActivityLabels, "ActivityN")[, 2]
+
+names(SubjectData) <- "Subject"
+names(FeaturesData) <- FeaturesNames$V2
+
+DataSet <- cbind(SubjectData, Activity)
+DataSet <- cbind(DataSet, FeaturesData)
+
+subFeaturesNames <- FeaturesNames$V2[grep("mean\\(\\)|std\\(\\)", FeaturesNames$V2)]
+DataNames <- c("Subject", "Activity", as.character(subFeaturesNames))
+DataSet <- subset(DataSet, select=DataNames)
+
+names(DataSet)<-gsub("^t", "time", names(DataSet))
+names(DataSet)<-gsub("^f", "frequency", names(DataSet))
+names(DataSet)<-gsub("Acc", "Accelerometer", names(DataSet))
+names(DataSet)<-gsub("Gyro", "Gyroscope", names(DataSet))
+names(DataSet)<-gsub("Mag", "Magnitude", names(DataSet))
+names(DataSet)<-gsub("BodyBody", "Body", names(DataSet))
+
+SecondDataSet<-aggregate(. ~Subject + Activity, DataSet, mean)
+SecondDataSet<-SecondDataSet[order(SecondDataSet$Subject,SecondDataSet$Activity),]
+
+write.table(SecondDataSet, file = "tidydata.txt",row.name=FALSE)
